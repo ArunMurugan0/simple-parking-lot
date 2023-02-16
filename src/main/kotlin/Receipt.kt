c336d17e2efaaa99954b28ab4a120e65c12b0f14
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-17 01:42:39 +0530

    Add Receipt Number getter

--- src/main/kotlin/Receipt.kt
+++ src/main/kotlin/Receipt.kt
@@ -1,3 +1,6 @@
 import java.time.LocalDateTime
 
-data class Receipt(val ticket: Ticket, val exitDateTime: LocalDateTime, val fee: UInt)
+data class Receipt(val ticket: Ticket, val exitDateTime: LocalDateTime, val fee: UInt) {
+    val receiptNumber
+            get() = "R-${ticket.ticketNumber.value}"
+}
