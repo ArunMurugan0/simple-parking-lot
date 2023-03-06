3b8a7a89561f2c932fac393b164418473137dc77
Arun Murugan <arunmurugan@sahaj.ai>
2023-03-06 13:17:17 +0530

    add annotation to use all Open plugin to make clasess open on annotating with Open Class

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.21"
+}
+
+allOpen {
+    annotation("OpenClass")
 }
 
 group = "org.example"
@@ -14,6 +19,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.1.0")
+    testImplementation("org.mockito:mockito-core:2.1.0")
 }
 
 tasks.test {
